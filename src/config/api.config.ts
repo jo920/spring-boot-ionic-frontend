a9c55a9c479e11be9868f19c43e8a174be05d881
Joao Henrique <joaohenrique.reis18@gmail.com>
2022-12-30 14:19:39 -0300

    Mostrando as Categorias version 1.0

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,6 @@
 export const API_CONFIG ={
 
-baseUrl : "http://localhost:8080"
-
+baseUrl : "http://localhost:8080",
+//ainda estou implementando
+buckedUrl :"https://springboot-ionic18.s3.sa-east-1.amazonaws.com/"
 }
